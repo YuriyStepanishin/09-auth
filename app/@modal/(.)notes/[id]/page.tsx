cfa0ef9ebd067e39b353f193be891f6fe690068b
YuriyStepanishin <y.stepanishin@gmail.com>
2026-04-18 16:17:05 +0300

    fix modal

---
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -0,0 +1,29 @@
+export { default } from "@/app/(private routes)/@modal/(.)notes/[id]/page";
+import {
+  QueryClient,
+  dehydrate,
+  HydrationBoundary,
+} from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api/serverApi";
+import NotePreview from "./NotePreview.client";
+
+export default async function NoteModal({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePreview id={id} />
+    </HydrationBoundary>
+  );
+}
---
+++ app/@modal/default.tsx
@@ -0,0 +1,1 @@
+export { default } from "@/app/(private routes)/@modal/default";
